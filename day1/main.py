fdaf04b2ebedbc035cfbea4e7e6601a38c036545
Vanbien2525 <binnguyen25102k5@gmail.com>
2025-08-27 18:06:32 +0700

    uppdate

--- day1/main.py
+++ day1/main.py
@@ -4,5 +4,14 @@ print("😊❤️")
 print(1, 2, 3, 4, 5, sep = " % ", end = " ")
 print(6)
 print("day la dong moi")
+print("chào các bạn")
 print("day la dong moi 2")
 # ctrl + / : comment code
+
+full_name = input("Nhap vao ten cua ban: ")
+print(type(full_name))
+print(full_name)
+
+number =int(input("Nhap so nguyen: "))
+print(type(number))
+print(number)
