e0fa1b568eb7d5fc188eec91c558c68548666bc1
Vanbien2525 <binnguyen25102k5@gmail.com>
2025-11-18 00:58:20 +0700

    date7

---
+++ day7/if_else.py
@@ -0,0 +1,90 @@
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+day = 3
+
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+
+temperature = 22
+
+if temperature > 30:
+  print("It's hot outside!")
+elif temperature > 20:
+  print("It's warm outside")
+elif temperature > 10:
+  print("It's cool outside")
+else:
+  print("It's cold outside!")
+
+
+a = 200
+b = 33
+if a > b: print("a is greater than b")
+
+
+# The syntax follows this pattern:
+#     variable = value_if_true if condition else value_if_false
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+age = 16
+
+if age < 18:
+  pass # TODO: Add underage logic later
+else:
+  print("Access granted")
